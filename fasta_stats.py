c832b09326200b127f9a701da8ca15890e9f5d59
Konstantinos Patsis <patsis.csd@gmail.com>
2014-06-05 20:35:49 +0200

    modified:   fasta_stats.py

--- fasta_stats.py
+++ fasta_stats.py
@@ -4,6 +4,10 @@ from pbcore.io import FastaReader
 
 f = FastaReader(sys.argv[1])
 
+sum_bp = -1
+if len(sys.argv) == 3:
+	sum_bp = int(sys.argv[2])
+
 sum = 0
 lenlist = []
 more1k = 0
@@ -30,9 +34,15 @@ minlen = lenlist[0]
 lenlist.reverse()
 maxlen = lenlist[0]
 
+# compute N50/90 based on genome size
+if sum_bp != -1:
+	sum_tmp = sum_bp
+else:
+	sum_tmp = sum
+
 for rl in lenlist:
     rsum = rsum + rl
-    if rsum > sum/2:
+    if rsum > sum_tmp/2:
         n50 = rl
         break
 
@@ -40,7 +50,7 @@ rsum = 0
 
 for rl in lenlist:
     rsum = rsum + rl
-    if rsum > sum*0.9:
+    if rsum > sum_tmp*0.9:
         n90 = rl
         break
 
