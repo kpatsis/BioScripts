78abfdd1c36b95438aab5957027075d6645fec37
Konstantinos Patsis <patsis.csd@gmail.com>
2014-06-16 14:29:49 +0200

    modified:   fix_tags_contigs.py

--- fix_tags_contigs.py
+++ fix_tags_contigs.py
@@ -3,14 +3,20 @@ import sys
 import random
 from subprocess import call
 
-if (sys.argv[1].split('.').pop() == "fastq") or (sys.argv[1].split('.').pop() == "fq"):
+fname = sys.argv[1]
+fname_list = fname.split('.')
+fext = fname_list.pop()
+fname_pure = ""
+for el in fname_list:
+    fname_pure = fname_pure + el + '.'
+
+if (fext == "fastq") or (fext == "fq"):
         awk = "awk \'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,\">\");print}; if(P==4)P=0; P++}\' "
-        new_name = sys.argv[1].split('.')[0]+".fasta"
-        awk = awk +  sys.argv[1] + " > " + new_name
+        new_name = fname_pure+"fasta"
+        awk = awk +  fname + " > " + new_name
         call(awk,shell=True)
-        call(["rm",sys.argv[1]])
-else:
-	new_name = sys.argv[1]
+        #call(["rm",sys.argv[1]])
+
 
 f = open(new_name,'r')
 wf = open(sys.argv[1]+"_fixed",'w')
